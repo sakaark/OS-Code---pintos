191a57ca486c4818cec977e9d91027b1d6fe0126
Sakaar Khurana <sakaar.iitk@gmail.com>
2012-09-30 09:50:11 +0530

    got the idea for termination

--- src/tests/threads/mytest.c
+++ src/tests/threads/mytest.c
@@ -5,8 +5,26 @@
 #include "lib/debug.h"
 #include "userprog/syscall.h"
 
+void printing2(void){
+  printf("harrah 2!!\n");
+  thread_exit();
+}
+
 void printing(void){
+  pthread_t *t;
+  pthread_attr_t x;
+  x.detachstate = DETACHED;
+  x.inheritsched = 1;
+  x.schedpolicy = SCHED_FCFS;
+  x.sched_priority = 31;
+  t = malloc(sizeof(pthread_t));
+  //halt();
+  //debug_backtrace_all();
+  //halt();
+  pthread_create(t, &x, &printing2, NULL);
+
   printf("hurrah!!\n");
+  thread_exit();
 }
 
 void test_mytest(){
